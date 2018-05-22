981140e8fb7b9d8386b7ad13610df9318bbd7d2f
youyuxun <youyuxun@leoao.com>
2018-05-22 18:21:06 +0800

    add generate function

--- bin/lefit.js
+++ bin/lefit.js
@@ -1,17 +1,22 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const chalk = require('chalk');
 const pkg = require('../package.json');
 
 const newCli = require('../lib/new')
+const newGenerate = require('../lib/generate')
 
 program.version(pkg.version)
   .option('new', 'new a lefit-cms project')
+  .option('n', 'new a lefit-cms project')
   .option('new --no-auto-install', 'new a lefit-cms project')
+  .option('n --no-auto-install', 'new a lefit-cms project')
+  .option('g', 'new a component')
   .parse(process.argv);
 
-if (program.new) {
+if (program.new || program.n) {
   newCli(process.argv)
-  // console.log(process.argv)
+}
+if (program.g) {
+  newGenerate(process.argv)
 }
